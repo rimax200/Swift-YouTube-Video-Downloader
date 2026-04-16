8e7fc1c8402d62117e92cd9ee65770bf80eca690
Maxwell <ebirim.max321@gmail.com>
2026-04-16 17:55:32 +0100

    Initial Commit

---
+++ RXDownloader/RXDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXDownloaderApp.swift
+//  RXDownloader
+//
+//  Created by Maxwell on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct RXDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RXDownloaderTests/RXDownloaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RXDownloaderTests.swift
+//  RXDownloaderTests
+//
+//  Created by Maxwell on 4/16/26.
+//
+
+import Testing
+@testable import RXDownloader
+
+struct RXDownloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
